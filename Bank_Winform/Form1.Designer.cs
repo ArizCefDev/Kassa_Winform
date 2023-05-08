3d3340684e38458d0b0a97c628a0df71de96fd2c
ArizCefDev <106917027+ArizCefDev@users.noreply.github.com>
2023-05-08 21:55:29 +0400

    Add project files.

---
+++ Bank_Winform/Form1.Designer.cs
@@ -0,0 +1,425 @@
+﻿namespace Bank_Winform
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new System.Windows.Forms.Button();
+            label1 = new System.Windows.Forms.Label();
+            textBox1 = new System.Windows.Forms.TextBox();
+            listBox1 = new System.Windows.Forms.ListBox();
+            label2 = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
+            textBox2 = new System.Windows.Forms.TextBox();
+            label5 = new System.Windows.Forms.Label();
+            label6 = new System.Windows.Forms.Label();
+            label7 = new System.Windows.Forms.Label();
+            label8 = new System.Windows.Forms.Label();
+            groupBox1 = new System.Windows.Forms.GroupBox();
+            button10 = new System.Windows.Forms.Button();
+            button11 = new System.Windows.Forms.Button();
+            button12 = new System.Windows.Forms.Button();
+            button9 = new System.Windows.Forms.Button();
+            button8 = new System.Windows.Forms.Button();
+            button7 = new System.Windows.Forms.Button();
+            button6 = new System.Windows.Forms.Button();
+            button5 = new System.Windows.Forms.Button();
+            button4 = new System.Windows.Forms.Button();
+            button3 = new System.Windows.Forms.Button();
+            button2 = new System.Windows.Forms.Button();
+            button13 = new System.Windows.Forms.Button();
+            label4 = new System.Windows.Forms.Label();
+            button14 = new System.Windows.Forms.Button();
+            pictureBox1 = new System.Windows.Forms.PictureBox();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            button1.ForeColor = System.Drawing.Color.Black;
+            button1.Location = new System.Drawing.Point(63, 80);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(66, 55);
+            button1.TabIndex = 0;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.ForeColor = System.Drawing.Color.White;
+            label1.Location = new System.Drawing.Point(107, 76);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(110, 21);
+            label1.TabIndex = 1;
+            label1.Text = "Mehsul Kodu";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new System.Drawing.Point(107, 115);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new System.Drawing.Size(110, 29);
+            textBox1.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 21;
+            listBox1.Location = new System.Drawing.Point(546, 205);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new System.Drawing.Size(386, 277);
+            listBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.ForeColor = System.Drawing.Color.White;
+            label2.Location = new System.Drawing.Point(670, 76);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(133, 21);
+            label2.TabIndex = 4;
+            label2.Text = "Ümumi məbləğ:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.ForeColor = System.Drawing.Color.White;
+            label3.Location = new System.Drawing.Point(821, 76);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(94, 21);
+            label3.TabIndex = 5;
+            label3.Text = "0.00 manat";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new System.Drawing.Point(63, 33);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new System.Drawing.Size(288, 29);
+            textBox2.TabIndex = 8;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.ForeColor = System.Drawing.Color.White;
+            label5.Location = new System.Drawing.Point(821, 114);
+            label5.Name = "label5";
+            label5.Size = new System.Drawing.Size(94, 21);
+            label5.TabIndex = 10;
+            label5.Text = "0.00 manat";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.ForeColor = System.Drawing.Color.White;
+            label6.Location = new System.Drawing.Point(657, 114);
+            label6.Name = "label6";
+            label6.Size = new System.Drawing.Size(146, 21);
+            label6.TabIndex = 9;
+            label6.Text = "Ödənilən məbləğ:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.ForeColor = System.Drawing.Color.White;
+            label7.Location = new System.Drawing.Point(821, 153);
+            label7.Name = "label7";
+            label7.Size = new System.Drawing.Size(94, 21);
+            label7.TabIndex = 12;
+            label7.Text = "0.00 manat";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.ForeColor = System.Drawing.Color.White;
+            label8.Location = new System.Drawing.Point(651, 153);
+            label8.Name = "label8";
+            label8.Size = new System.Drawing.Size(156, 21);
+            label8.TabIndex = 11;
+            label8.Text = "Qaytarılan məbləğ:";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button10);
+            groupBox1.Controls.Add(button11);
+            groupBox1.Controls.Add(button12);
+            groupBox1.Controls.Add(button9);
+            groupBox1.Controls.Add(button8);
+            groupBox1.Controls.Add(button7);
+            groupBox1.Controls.Add(button6);
+            groupBox1.Controls.Add(button5);
+            groupBox1.Controls.Add(button4);
+            groupBox1.Controls.Add(button3);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(button1);
+            groupBox1.ForeColor = System.Drawing.Color.White;
+            groupBox1.Location = new System.Drawing.Point(37, 205);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new System.Drawing.Size(418, 277);
+            groupBox1.TabIndex = 21;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Ödəniş et";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // button10
+            // 
+            button10.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            button10.ForeColor = System.Drawing.Color.Black;
+            button10.Location = new System.Drawing.Point(284, 202);
+            button10.Name = "button10";
+            button10.Size = new System.Drawing.Size(67, 55);
+            button10.TabIndex = 19;
+            button10.Text = "0";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += button10_Click;
+            // 
+            // button11
+            // 
+            button11.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            button11.ForeColor = System.Drawing.Color.Black;
+            button11.Location = new System.Drawing.Point(284, 141);
+            button11.Name = "button11";
+            button11.Size = new System.Drawing.Size(67, 55);
+            button11.TabIndex = 18;
+            button11.Text = ".";
+            button11.UseVisualStyleBackColor = true;
+            button11.Click += button11_Click;
+            // 
+            // button12
+            // 
+            button12.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            button12.ForeColor = System.Drawing.Color.Black;
+            button12.Location = new System.Drawing.Point(284, 80);
+            button12.Name = "button12";
+            button12.Size = new System.Drawing.Size(67, 55);
+            button12.TabIndex = 17;
+            button12.Text = "Enter";
+            button12.UseVisualStyleBackColor = true;
+            button12.Click += button12_Click;
+            // 
+            // button9
+            // 
+            button9.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            button9.ForeColor = System.Drawing.Color.Black;
+            button9.Location = new System.Drawing.Point(210, 202);
+            button9.Name = "button9";
+            button9.Size = new System.Drawing.Size(68, 55);
+            button9.TabIndex = 16;
+            button9.Text = "9";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
+            // 
+            // button8
+            // 
+            button8.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            button8.ForeColor = System.Drawing.Color.Black;
+            button8.Location = new System.Drawing.Point(135, 202);
+            button8.Name = "button8";
+            button8.Size = new System.Drawing.Size(69, 55);
+            button8.TabIndex = 15;
+            button8.Text = "8";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
+            // 
+            // button7
+            // 
+            button7.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            button7.ForeColor = System.Drawing.Color.Black;
+            button7.Location = new System.Drawing.Point(63, 202);
+            button7.Name = "button7";
+            button7.Size = new System.Drawing.Size(66, 55);
+            button7.TabIndex = 14;
+            button7.Text = "7";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button6
+            // 
+            button6.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            button6.ForeColor = System.Drawing.Color.Black;
+            button6.Location = new System.Drawing.Point(210, 141);
+            button6.Name = "button6";
+            button6.Size = new System.Drawing.Size(68, 55);
+            button6.TabIndex = 13;
+            button6.Text = "6";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button5
+            // 
+            button5.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            button5.ForeColor = System.Drawing.Color.Black;
+            button5.Location = new System.Drawing.Point(135, 141);
+            button5.Name = "button5";
+            button5.Size = new System.Drawing.Size(69, 55);
+            button5.TabIndex = 12;
+            button5.Text = "5";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button4
+            // 
+            button4.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            button4.ForeColor = System.Drawing.Color.Black;
+            button4.Location = new System.Drawing.Point(63, 141);
+            button4.Name = "button4";
+            button4.Size = new System.Drawing.Size(66, 55);
+            button4.TabIndex = 11;
+            button4.Text = "4";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            button3.ForeColor = System.Drawing.Color.Black;
+            button3.Location = new System.Drawing.Point(210, 80);
+            button3.Name = "button3";
+            button3.Size = new System.Drawing.Size(68, 55);
+            button3.TabIndex = 10;
+            button3.Text = "3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            button2.ForeColor = System.Drawing.Color.Black;
+            button2.Location = new System.Drawing.Point(135, 80);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(69, 55);
+            button2.TabIndex = 9;
+            button2.Text = "2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button13
+            // 
+            button13.Location = new System.Drawing.Point(232, 113);
+            button13.Name = "button13";
+            button13.Size = new System.Drawing.Size(137, 31);
+            button13.TabIndex = 22;
+            button13.Text = "Əlavə et";
+            button13.UseVisualStyleBackColor = true;
+            button13.Click += button13_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.ForeColor = System.Drawing.Color.Red;
+            label4.Location = new System.Drawing.Point(53, 181);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(113, 21);
+            label4.TabIndex = 23;
+            label4.Text = "errormessage";
+            label4.Visible = false;
+            // 
+            // button14
+            // 
+            button14.Location = new System.Drawing.Point(232, 76);
+            button14.Name = "button14";
+            button14.Size = new System.Drawing.Size(137, 31);
+            button14.TabIndex = 24;
+            button14.Text = "Kod siyahisi";
+            button14.UseVisualStyleBackColor = true;
+            button14.Click += button14_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new System.Drawing.Point(434, 64);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new System.Drawing.Size(112, 98);
+            pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 25;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            BackColor = System.Drawing.Color.FromArgb(0, 64, 64);
+            ClientSize = new System.Drawing.Size(957, 502);
+            Controls.Add(pictureBox1);
+            Controls.Add(button14);
+            Controls.Add(label4);
+            Controls.Add(button13);
+            Controls.Add(groupBox1);
+            Controls.Add(label7);
+            Controls.Add(label8);
+            Controls.Add(label5);
+            Controls.Add(label6);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(listBox1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            Margin = new System.Windows.Forms.Padding(4);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ Bank_Winform/Form1.cs
@@ -0,0 +1,177 @@
+﻿using MessagingToolkit.QRCode.Codec;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bank_Winform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Product p = new Product();
+        decimal total = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+
+            if (textBox1.Text == "122")
+            {
+                object[] list = { p.Name = "Lays", p.Price = 2.99M };
+
+                foreach (var item in list)
+                {
+                    listBox1.Items.Add(item);
+                }
+                total = total + p.Price;
+
+            }
+            else if (textBox1.Text == "123")
+            {
+                object[] list = { p.Name = "Coca-cola", p.Price = 1.19M };
+
+                foreach (var item in list)
+                {
+                    listBox1.Items.Add(item);
+                }
+
+                total = total + p.Price;
+            }
+
+            else if (textBox1.Text == "342")
+            {
+                object[] list = { p.Name = "Kurassan", p.Price = 1.39M };
+
+                foreach (var item in list)
+                {
+                    listBox1.Items.Add(item);
+                }
+
+                total = total + p.Price;
+            }
+            label3.Text = total.ToString() + " " + "manat";
+
+            QRCodeEncoder encoder = new QRCodeEncoder();
+            pictureBox1.Image = encoder.Encode(textBox1.Text);
+
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+                "122: Lays \n " +
+                "123: Coca-Cola \n" +
+                "342: Kurassan \n"
+                );
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+
+            decimal money = Convert.ToDecimal(textBox2.Text);
+
+            if (money < total)
+            {
+                label4.Visible = true;
+                label4.Text = "Ödəniləcək məbləğ tutardan az ola bilməz";
+                label5.ForeColor = Color.Red;
+                label5.Text = money.ToString() + " " + "manat";
+            }
+            else
+            {
+                decimal res = money - total;
+                label7.Text = res.ToString() + " " + "manat";
+                label4.Visible = false;
+                label5.ForeColor = Color.White;
+                label5.Text = money.ToString() + " " + "manat";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            textBox2.Text += btn.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            textBox2.Text += btn.Text;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            textBox2.Text += btn.Text;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            textBox2.Text += btn.Text;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            textBox2.Text += btn.Text;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            textBox2.Text += btn.Text;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            textBox2.Text += btn.Text;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            textBox2.Text += btn.Text;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            textBox2.Text += btn.Text;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            textBox2.Text += btn.Text;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            textBox2.Text += btn.Text;
+        }
+    }
+}
+
